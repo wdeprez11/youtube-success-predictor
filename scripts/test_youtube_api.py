4c9da49b09016cc257e67140eed467c4727380d2
William Deprez <w.deprez11@gmail.com>
2026-01-13 14:47:25 -0600

    Corrected logic to properly acquire video IDs from uploads playlist of specified CHANNEL_ID

--- scripts/test_youtube_api.py
+++ scripts/test_youtube_api.py
@@ -8,36 +8,49 @@ API_KEY = os.getenv("YOUTUBE_API_KEY")
 CHANNEL_ID = "UCNUzWfHUP_iXZ1GMHz8gBgw"
 URL = "https://www.googleapis.com/youtube/v3/search"
 
-videos = []
+PLAYLIST_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+
+
+CHANNELS_URL = "https://www.googleapis.com/youtube/v3/channels"
+
+params = {
+    "key": API_KEY,
+    "id": CHANNEL_ID,
+    "part": "contentDetails"
+}
+
+response = requests.get(CHANNELS_URL, params=params)
+data = response.json()
+
+uploads_playlist_id = data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+print("Uploads playlist ID:", uploads_playlist_id)
+
+video_ids = []
 next_page_token = None
 
-while len(videos) < 100:
+while len(video_ids)  < 100:
     params = {
         "key": API_KEY,
-        "channelId": CHANNEL_ID,
-        "part": "snippet",
-        "order": "date",
+        "playlistId": uploads_playlist_id,
+        "part": "contentDetails",
         "maxResults": 50,
-        "type": "video",
     }
 
     if next_page_token:
         params["pageToken"] = next_page_token
 
-    response = requests.get(URL, params=params)
+
+    response = requests.get(PLAYLIST_URL, params=params)
     data = response.json()
 
-    items = data.get("items", [])
-    for item in items:
-        videos.append(item["id"]["videoId"])
+    for item in data.get("items", []):
+        video_ids.append(item["contentDetails"]["videoId"])
 
     next_page_token = data.get("nextPageToken")
 
     if not next_page_token:
         break
 
-print(f"Returned {len(videos)} videos")
-print(videos[:10])
-
-# for video in videos[:10]:
-    # print(video["snippet"]["title"])
+print(f"Collected {len(video_ids)} video IDs")
+print(video_ids[:10])
