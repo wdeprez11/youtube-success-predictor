fda9294ad092b63b28cd2047d9e97a84554efd2f
William Deprez <w.deprez11@gmail.com>
2026-01-23 16:34:19 -0600

    Started training the baseline model with more data from DataFrame.

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 from dotenv import load_dotenv
 
 from src.pipeline.youtube_pipeline import run_pipeline
+from src.models.baseline_title_model import run_baseline
  
 def main():
     load_dotenv()
     api_key = os.getenv("YOUTUBE_API_KEY")
 
     df = run_pipeline(limit=200)
+    log_reg = run_baseline()
 
 if __name__ == "__main__":
     main()
--- src/models/baseline_title_model.py
+++ src/models/baseline_title_model.py
@@ -3,26 +3,54 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot  as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
-df = pd.read_csv("ryukahr_videos.csv")
+def run_baseline() -> LogisticRegression:
+    df = pd.read_csv("ryukahr_videos.csv")
 
-X = df["title"]
-y = df["successful"]
+    FEATURE_COLUMNS = [
+        "publish_hour",
+        "publish_weekday",
+        "duration_seconds",
+    ]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
-)
+    X = df[["title"] + FEATURE_COLUMNS]
+    y = df["successful"]
 
-vectorizer = TfidfVectorizer(
-    max_features=3000,
-    stop_words="english"
-)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
 
-X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("title", TfidfVectorizer(max_features=3000, stop_words="english"), "title"),
+            ("num", StandardScaler(), FEATURE_COLUMNS)
+        ]
+    )
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train_vec, y_train)
+    model = LogisticRegression(max_iter=1000, class_weight="balanced")
 
-y_pred = model.predict(X_test_vec)
-print(classification_report(y_test, y_pred))
+    pipeline = Pipeline([
+        ("preprocess", preprocessor),
+        ("model", model),
+    ])
+
+    pipeline.fit(X_train, y_train)
+
+    y_pred = pipeline.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+    plt.title("Baseline Title Model Confusion Matrix")
+    plt.show()
+
+    return model
+
+if __name__ == "__main__":
+    run_baseline()
